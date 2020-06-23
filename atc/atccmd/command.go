3136033fc280b17d91953db9fded91827eaffdc3
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-06-23 09:16:59 -0400

    api: behaviour: ListAllJobs endpoint supports watching
    concourse/rfcs#61
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -38,6 +38,7 @@ import (
 	"github.com/concourse/concourse/atc/db/encryption"
 	"github.com/concourse/concourse/atc/db/lock"
 	"github.com/concourse/concourse/atc/db/migration"
+	"github.com/concourse/concourse/atc/db/watch"
 	"github.com/concourse/concourse/atc/engine"
 	"github.com/concourse/concourse/atc/engine/builder"
 	"github.com/concourse/concourse/atc/gc"
@@ -781,6 +782,11 @@ func (cmd *RunCommand) constructAPIMembers(
 
 	middleware := token.NewMiddleware(cmd.Auth.AuthFlags.SecureCookies)
 
+	listAllJobsWatcher, err := watch.NewListAllJobsWatcher(logger.Session("list-all-jobs-watcher"), dbConn, lockFactory)
+	if err != nil {
+		return nil, err
+	}
+
 	apiHandler, err := cmd.constructAPIHandler(
 		logger,
 		reconfigurableSink,
@@ -802,6 +808,7 @@ func (cmd *RunCommand) constructAPIMembers(
 		accessFactory,
 		dbWall,
 		policyChecker,
+		listAllJobsWatcher,
 	)
 	if err != nil {
 		return nil, err
@@ -1805,6 +1812,7 @@ func (cmd *RunCommand) constructAPIHandler(
 	accessFactory accessor.AccessFactory,
 	dbWall db.Wall,
 	policyChecker *policy.Checker,
+	listAllJobsWatcher *watch.ListAllJobsWatcher,
 ) (http.Handler, error) {
 
 	checkPipelineAccessHandlerFactory := auth.NewCheckPipelineAccessHandlerFactory(teamFactory)
@@ -1893,6 +1901,8 @@ func (cmd *RunCommand) constructAPIHandler(
 		time.Minute,
 		dbWall,
 		clock.NewClock(),
+
+		listAllJobsWatcher,
 	)
 }
 
